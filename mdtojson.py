b8077cbcccf69a76a986446b23b6ef6f731bead3
miijane <mijane.404@gmail.com>
2023-10-29 08:26:12 +0800

    initial commit

---
+++ mdtojson.py
@@ -0,0 +1,27 @@
+import os 
+import glob
+import json
+import markdown
+
+quote_files = glob.glob("quotes/**")
+
+for quote_file in quote_files:
+    print(quote_file)
+    f = open(quote_file, 'r')
+    print(markdown.markdown(f.read()))
+
+def write_json_data():
+    with open("db.json", "w") as json_file:
+        json.dump(
+            {
+                "quotes": [
+                    {
+                        "text": "first trial"
+                    }
+                ]
+            },
+            json_file,
+            indent=4
+        )
+
+write_json_data()
---
+++ quotes/a.md
@@ -0,0 +1,5 @@
+---
+tags: ["quotes"]
+---
+
+hello world
